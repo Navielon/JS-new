161f0dfd70c6305a02d6a9c037804b9557b016af
Ruslan_gg <107236487+Navielon@users.noreply.github.com>
2022-07-19 14:34:30 +0300

    upd

--- app.js
+++ app.js
@@ -13,6 +13,7 @@
 // const withNum5 = 5
 
 // const 5withNum = 5  err
+//!========================================================================================================================================================
 
 // 2 Мутирование
 
@@ -22,32 +23,48 @@
 // const lastName = prompt('Введите фамилию');
 // console.log(firstName + lastName + age);
 
+//!========================================================================================================================================================
 
 // 3 Операторы
 
 // const currentYear = 2022
-let currentYear = 2022
-const birthYear = 2000
+// let currentYear = 2022
+// const birthYear = 2000
 
 // const age = currentYear - birthYear;
 
-const a = 10
-const b = 5
-let c = 32
+// const a = 10
+// const b = 5
+// let c = 32
 
 
-c += a // c = c + a
-c -= a // c = c - a
-c *= a // c = c * a
-c /= a // c = c / a
+// c += a // c = c + a
+// c -= a // c = c - a
+// c *= a // c = c * a
+// c /= a // c = c / a
 
-// console.log(age);
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
+//// console.log(age);
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
 
-// console.log(currentYear++); // Инкриминтирует значение на 1 после значения
-console.log(++currentYear); // Инкриминтирует значение на 1 перед значения
-console.log(currentYear);
-console.log(c);
+// // console.log(currentYear++); // Инкриминтирует значение на 1 после значения
+// console.log(++currentYear); // Инкриминтирует значение на 1 перед значения
+// console.log(currentYear);
+// console.log(c);
+//!========================================================================================================================================================
+
+// 4. Типы данных
+
+// const isProgrammer = true; // Первый (Boolean)
+// const name = 'Руслан'; // Второй (string)
+// const age = 22; // Третий (number)
+// let x // Четвертый (undefind)
+// null // Пятый (object)
+// console.log(typeof isProgrammer); // Тип данных
+// console.log(typeof name); // Тип данных
+// console.log(typeof age); // Тип данных
+// console.log(typeof x); // Тип данных
+// console.log(typeof null); // Тип данных
+//!========================================================================================================================================================
