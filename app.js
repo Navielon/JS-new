0b15efde917da1846680fdc466a8d6abcdee5731
Ruslan_gg <107236487+Navielon@users.noreply.github.com>
2022-07-19 14:27:35 +0300

    new

---
+++ app.js
@@ -0,0 +1,53 @@
+// 1 Переменные JavaScript
+// var name = 'Ruslan'; // Не используется
+
+// let lastName = 'Gavrilin' // Используется
+// const firstName = 'Ruslan' // Используется
+// let age = 22;
+
+// age = 25
+// const $ = 'private'
+// const _ = 'private'
+// const if = 'private' err
+
+// const withNum5 = 5
+
+// const 5withNum = 5  err
+
+// 2 Мутирование
+
+// console.log('Имя человека: ' + firstName + ' ' + ', а имя человека: ' + age);
+// alert('Имя человека: ' + firstName + ' ' + ', а имя человека: ' + age);
+
+// const lastName = prompt('Введите фамилию');
+// console.log(firstName + lastName + age);
+
+
+// 3 Операторы
+
+// const currentYear = 2022
+let currentYear = 2022
+const birthYear = 2000
+
+// const age = currentYear - birthYear;
+
+const a = 10
+const b = 5
+let c = 32
+
+
+c += a // c = c + a
+c -= a // c = c - a
+c *= a // c = c * a
+c /= a // c = c / a
+
+// console.log(age);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+// console.log(currentYear++); // Инкриминтирует значение на 1 после значения
+console.log(++currentYear); // Инкриминтирует значение на 1 перед значения
+console.log(currentYear);
+console.log(c);
