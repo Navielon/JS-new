ac9cb919a30ad240fafebbff30ae81f3337ca435
Ruslan_gg <107236487+Navielon@users.noreply.github.com>
2022-07-19 17:13:24 +0300

    upd

--- app.js
+++ app.js
@@ -1,265 +1,38 @@
-// 1 Переменные JavaScript
-// var name = 'Ruslan'; // Не используется
+// Числа (Number)
 
-// let lastName = 'Gavrilin' // Используется
-// const firstName = 'Ruslan' // Используется
-// let age = 22;
+// const num = 42 // integer
+// const float = 42.42 // float
+// const pow = 10e3 // Возводим в третью степень
+// console.log(num);
+// console.log(float);
+// console.log(pow);
 
-// age = 25
-// const $ = 'private'
-// const _ = 'private'
-// const if = 'private' err
+// console.log('MAX_SAFE_INTEGER', Number.MAX_SAFE_INTEGER);
+// console.log('Math.pow', Math.pow(2, 53) -1);
+// console.log('MIN_SAFE_INTEGER', Number.MIN_SAFE_INTEGER);
+// console.log('MAX_VALUE', Number.MAX_VALUE);
+// console.log('MIN_VALUE', Number.MIN_VALUE);
+// console.log('POSITIVE_INFINITY', Number.POSITIVE_INFINITY);
+// console.log('NEGATIVE_INFINITY', Number.NEGATIVE_INFINITY);
+// console.log('1 / 0', 2 / 0); // Infinite
+// console.log(Number.NaN); // Not A Number
+// console.log(typeof NaN); // type = Number
+// console.log(2 / undefined); // NaN
+// const weird = 2 / undefined; // NaN
+// console.log(Number.isNaN(weird)); // True
+// console.log(Number.isNaN(42)); // False
+// console.log(Number.isFinite(Infinity)); // Конечна ли бесконечность - False
+// console.log(Number.isFinite(42)); // Конечно ли 42 - True
 
-// const withNum5 = 5
+// const stringInt = '40';
+// const stringFloat = '40.42';
+// console.log(Number.parseInt(stringInt) + 2); // 42
+// console.log(parseInt(stringInt) + 2); // 42
+// console.log(Number(stringInt) + 2); // 42
+// console.log(+stringInt + 2); // 42
 
-// const 5withNum = 5  err
-//!========================================================================================================================================================
+// console.log(parseInt(stringFloat) + 2); // 42
+// console.log(parseFloat(stringFloat) + 2); // 42.42
+// console.log(+stringFloat + 2); // 42.42
 
-// 2 Мутирование
-
-// console.log('Имя человека: ' + firstName + ' ' + ', а имя человека: ' + age);
-// alert('Имя человека: ' + firstName + ' ' + ', а имя человека: ' + age);
-
-// const lastName = prompt('Введите фамилию');
-// console.log(firstName + lastName + age);
-
-//!========================================================================================================================================================
-
-// 3 Операторы
-
-// const currentYear = 2022
-// let currentYear = 2022
-// const birthYear = 2000
-
-// const age = currentYear - birthYear;
-
-// const a = 10
-// const b = 5
-// let c = 32
-
-
-// c += a // c = c + a
-// c -= a // c = c - a
-// c *= a // c = c * a
-// c /= a // c = c / a
-
-//// console.log(age);
-// console.log(a + b);
-// console.log(a - b);
-// console.log(a * b);
-// console.log(a / b);
-
-// // console.log(currentYear++); // Инкриминтирует значение на 1 после значения
-// console.log(++currentYear); // Инкриминтирует значение на 1 перед значения
-// console.log(currentYear);
-// console.log(c);
-//!========================================================================================================================================================
-
-// 4. Типы данных
-
-// const isProgrammer = true; // Первый (Boolean)
-// const name = 'Руслан'; // Второй (string)
-// const age = 22; // Третий (number)
-// let x // Четвертый (undefind)
-// null // Пятый (object)
-// console.log(typeof isProgrammer); // Тип данных
-// console.log(typeof name); // Тип данных
-// console.log(typeof age); // Тип данных
-// console.log(typeof x); // Тип данных
-// console.log(typeof null); // Тип данных
-//!========================================================================================================================================================
-
-// 5. Приоритет операторов
-
-// const fullAge = 22
-// const birthYear = 2000
-// const currentYear = 2022
-// >
-// <
-// >=
-// <=
-// const isFullAge = (currentYear - birthYear) >= fullAge // 22 >= 22 => true
-// console.log(isFullAge);
-//!========================================================================================================================================================
-
-// 6. Условные операторы
-
-// const courseStatus = 'fail'; // ready, fail, pending
-
-// if (courseStatus === 'ready') {
-//    console.log('Курс уже проходится, и скоро я стану джуном!');
-// } else if (courseStatus === 'pending') {
-//    console.log('Курс ещё не проходится, но я все равно скоро я стану джуном!');
-// } else if (courseStatus === 'fail') {
-//    console.log('Курс провален, но я все равно скоро я стану джуном!');
-// } else {
-//    console.log('Я в любом случае стану джуном. Или мидлом. Или тимлидом.')
-// }
-
-// Boolean значения
-// const isReady = true;
-
-// if (isReady === true) {
-//    console.log('Готово!');
-// }
-// // Нет смысла писать ()=== true)
-// if (isReady) {
-//    console.log('Готово!');
-// } else {
-//    console.log('Все не готово');
-// }
-
-
-// Тернарное выражение
-// isReady ? console.log('Все готово'): console.log('Все не готово');;
-
-
-// Что такое двойное равно (==) и тройное (===)
-// const num1 = 42
-// const num2 = '42'
-// console.log(num1 == num2); // True (Хотя тут разные типы данных! (Number и String))
-// console.log(num1 === num2); // False (Тут разные типы данных! (Number и String))
-//!========================================================================================================================================================
-
-// 7. Булевая логика
-
-// Логический оператор > И < (&&)
-
-// (True)
-// const a = true && true;
-// console.log(a);
-
-// (False)
-// const b = true && false;
-// console.log(b);
-
-// (False)
-// const c = false && true;
-// console.log(c);
-
-// (False)
-// const d = false && false;
-// console.log(d );
-
-
-// Логический оператор > ИЛИ < (||)
-
-// (True)
-// const a = true || true;
-// console.log(a);
-
-// (True)
-// const b = true || false;
-// console.log(b);
-
-// (True)
-// const c = false || true;
-// console.log(c);
-
-// (False)
-// const d = false || false;
-// console.log(d );
-
-
-// Логический оператор > НЕ < (!) Отрицание
-
-// (True)
-// const a = true;
-// console.log(a);
-
-// (False)
-// const b = !true;
-// console.log(b);
-
-// (True) 
-// const c = !!true;
-// console.log(c);
-
-// MDN
-// https://developer.mozilla.org/ru/docs/Web/JavaScript/Guide/Expressions_and_Operators
-//!========================================================================================================================================================
-
-// 8. Функции
-
-// function calculateAge (birthYear) {
-//    const currentYear = 2022
-//    return currentYear - birthYear;
-// }
-
-// console.log(calculateAge(2000));
-// console.log(calculateAge(2003));
-// console.log(calculateAge(1980));
-
-// function logInfoAbout (firstName, year){
-//    const age = calculateAge (year)
-//    if (age > 0) {
-//    console.log(`Человек по имени ${firstName}; Возраст :  ${age}`);
-//    } else {
-//       console.log('Человек ещё не родился!');
-//    }
-// }
-// logInfoAbout('Руслан', 2000)
-// logInfoAbout('Катя', 2003)
-// logInfoAbout('Катя', 2023)
-//!========================================================================================================================================================
-
-// 9. Массивы
-
-// Так делать не нужно
-//// const cars = new Array ('Мазда', 'Мерседес', 'Форд');
-//// console.log(cars);
-
-// const cars = ['Мазда', 'Мерседес', 'Форд'];
-// console.log(cars[0]);
-// console.log(cars[1]);
-// console.log(cars[2]);
-// console.log(cars.length);
-
-// cars [0] = 'Porsche';
-// cars [1] = 'BMW';
-// cars [2] = 'Lambargini';
-// cars [3] = 'Мазда';
-// cars [4] = 'Мерседес';
-// cars [5] = 'Форд';
-// cars [cars.length] = 'BIBIKA'; // В конец массива
-// console.log(cars);
-//!========================================================================================================================================================
-
-// 10. Циклы
-
-const cars = ['Мазда', 'Мерседес', 'Форд', 'Опель'];
-
-// for (let i = 0; i < cars.length; i++){
-//    const car = cars[i];
-//    console.log(car);
-// }
-
-// Новый способ в ES6
-for (let car of cars) {
-   console.log(car);
-}
-//!========================================================================================================================================================
-
-// 11. Объекты
-
-// const user = {
-//    firstName: 'Ruslan',
-//    lastName: 'Gavrilin',
-//    birthYear: 2000,
-//    languages: ['Ru', 'En', 'Bu'],
-//    hasWife: false,
-//    greet: function(){
-//       console.log('Greet!');
-//    }
-// }
-// Обращение к объекту
-// console.log(user.firstName);
-// console.log(user['lastName', 'birthYear']);
-// const key = 'languages'
-// console.log(user[key]);
-// user.hasWife = true;
-// console.log(user);
-// user.isProgrammer = true;
-// console.log(user);
-// user.greet();
 //!========================================================================================================================================================
---
+++ base.js
@@ -0,0 +1,265 @@
+// 1 Переменные JavaScript
+// var name = 'Ruslan'; // Не используется
+
+// let lastName = 'Gavrilin' // Используется
+// const firstName = 'Ruslan' // Используется
+// let age = 22;
+
+// age = 25
+// const $ = 'private'
+// const _ = 'private'
+// const if = 'private' err
+
+// const withNum5 = 5
+
+// const 5withNum = 5  err
+//!========================================================================================================================================================
+
+// 2 Мутирование
+
+// console.log('Имя человека: ' + firstName + ' ' + ', а имя человека: ' + age);
+// alert('Имя человека: ' + firstName + ' ' + ', а имя человека: ' + age);
+
+// const lastName = prompt('Введите фамилию');
+// console.log(firstName + lastName + age);
+
+//!========================================================================================================================================================
+
+// 3 Операторы
+
+// const currentYear = 2022
+// let currentYear = 2022
+// const birthYear = 2000
+
+// const age = currentYear - birthYear;
+
+// const a = 10
+// const b = 5
+// let c = 32
+
+
+// c += a // c = c + a
+// c -= a // c = c - a
+// c *= a // c = c * a
+// c /= a // c = c / a
+
+//// console.log(age);
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+
+// // console.log(currentYear++); // Инкриминтирует значение на 1 после значения
+// console.log(++currentYear); // Инкриминтирует значение на 1 перед значения
+// console.log(currentYear);
+// console.log(c);
+//!========================================================================================================================================================
+
+// 4. Типы данных
+
+// const isProgrammer = true; // Первый (Boolean)
+// const name = 'Руслан'; // Второй (string)
+// const age = 22; // Третий (number)
+// let x // Четвертый (undefind)
+// null // Пятый (object)
+// console.log(typeof isProgrammer); // Тип данных
+// console.log(typeof name); // Тип данных
+// console.log(typeof age); // Тип данных
+// console.log(typeof x); // Тип данных
+// console.log(typeof null); // Тип данных
+//!========================================================================================================================================================
+
+// 5. Приоритет операторов
+
+// const fullAge = 22
+// const birthYear = 2000
+// const currentYear = 2022
+// >
+// <
+// >=
+// <=
+// const isFullAge = (currentYear - birthYear) >= fullAge // 22 >= 22 => true
+// console.log(isFullAge);
+//!========================================================================================================================================================
+
+// 6. Условные операторы
+
+// const courseStatus = 'fail'; // ready, fail, pending
+
+// if (courseStatus === 'ready') {
+//    console.log('Курс уже проходится, и скоро я стану джуном!');
+// } else if (courseStatus === 'pending') {
+//    console.log('Курс ещё не проходится, но я все равно скоро я стану джуном!');
+// } else if (courseStatus === 'fail') {
+//    console.log('Курс провален, но я все равно скоро я стану джуном!');
+// } else {
+//    console.log('Я в любом случае стану джуном. Или мидлом. Или тимлидом.')
+// }
+
+// Boolean значения
+// const isReady = true;
+
+// if (isReady === true) {
+//    console.log('Готово!');
+// }
+// // Нет смысла писать ()=== true)
+// if (isReady) {
+//    console.log('Готово!');
+// } else {
+//    console.log('Все не готово');
+// }
+
+
+// Тернарное выражение
+// isReady ? console.log('Все готово'): console.log('Все не готово');;
+
+
+// Что такое двойное равно (==) и тройное (===)
+// const num1 = 42
+// const num2 = '42'
+// console.log(num1 == num2); // True (Хотя тут разные типы данных! (Number и String))
+// console.log(num1 === num2); // False (Тут разные типы данных! (Number и String))
+//!========================================================================================================================================================
+
+// 7. Булевая логика
+
+// Логический оператор > И < (&&)
+
+// (True)
+// const a = true && true;
+// console.log(a);
+
+// (False)
+// const b = true && false;
+// console.log(b);
+
+// (False)
+// const c = false && true;
+// console.log(c);
+
+// (False)
+// const d = false && false;
+// console.log(d );
+
+
+// Логический оператор > ИЛИ < (||)
+
+// (True)
+// const a = true || true;
+// console.log(a);
+
+// (True)
+// const b = true || false;
+// console.log(b);
+
+// (True)
+// const c = false || true;
+// console.log(c);
+
+// (False)
+// const d = false || false;
+// console.log(d );
+
+
+// Логический оператор > НЕ < (!) Отрицание
+
+// (True)
+// const a = true;
+// console.log(a);
+
+// (False)
+// const b = !true;
+// console.log(b);
+
+// (True) 
+// const c = !!true;
+// console.log(c);
+
+// MDN
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Guide/Expressions_and_Operators
+//!========================================================================================================================================================
+
+// 8. Функции
+
+// function calculateAge (birthYear) {
+//    const currentYear = 2022
+//    return currentYear - birthYear;
+// }
+
+// console.log(calculateAge(2000));
+// console.log(calculateAge(2003));
+// console.log(calculateAge(1980));
+
+// function logInfoAbout (firstName, year){
+//    const age = calculateAge (year)
+//    if (age > 0) {
+//    console.log(`Человек по имени ${firstName}; Возраст :  ${age}`);
+//    } else {
+//       console.log('Человек ещё не родился!');
+//    }
+// }
+// logInfoAbout('Руслан', 2000)
+// logInfoAbout('Катя', 2003)
+// logInfoAbout('Катя', 2023)
+//!========================================================================================================================================================
+
+// 9. Массивы
+
+// Так делать не нужно
+//// const cars = new Array ('Мазда', 'Мерседес', 'Форд');
+//// console.log(cars);
+
+// const cars = ['Мазда', 'Мерседес', 'Форд'];
+// console.log(cars[0]);
+// console.log(cars[1]);
+// console.log(cars[2]);
+// console.log(cars.length);
+
+// cars [0] = 'Porsche';
+// cars [1] = 'BMW';
+// cars [2] = 'Lambargini';
+// cars [3] = 'Мазда';
+// cars [4] = 'Мерседес';
+// cars [5] = 'Форд';
+// cars [cars.length] = 'BIBIKA'; // В конец массива
+// console.log(cars);
+//!========================================================================================================================================================
+
+// 10. Циклы
+
+const cars = ['Мазда', 'Мерседес', 'Форд', 'Опель'];
+
+// for (let i = 0; i < cars.length; i++){
+//    const car = cars[i];
+//    console.log(car);
+// }
+
+// Новый способ в ES6
+for (let car of cars) {
+   console.log(car);
+}
+//!========================================================================================================================================================
+
+// 11. Объекты
+
+// const user = {
+//    firstName: 'Ruslan',
+//    lastName: 'Gavrilin',
+//    birthYear: 2000,
+//    languages: ['Ru', 'En', 'Bu'],
+//    hasWife: false,
+//    greet: function(){
+//       console.log('Greet!');
+//    }
+// }
+// Обращение к объекту
+// console.log(user.firstName);
+// console.log(user['lastName', 'birthYear']);
+// const key = 'languages'
+// console.log(user[key]);
+// user.hasWife = true;
+// console.log(user);
+// user.isProgrammer = true;
+// console.log(user);
+// user.greet();
+//!========================================================================================================================================================
