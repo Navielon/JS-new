04b8fbc77d553e367e02b29d989352250e613dfa
Ruslan_gg <107236487+Navielon@users.noreply.github.com>
2022-07-19 20:01:17 +0300

    upd

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Числа (Number)
+// 1. Number
 
 // const num = 42 // integer
 // const float = 42.42 // float
@@ -45,13 +45,31 @@
 // console.log(parseFloat((0.4  + 0.2).toFixed(1))); // 0.6
 //!========================================================================================================================================================
 
-// BigInt
-console.log(9007199254740991999999n - 90071992547409919999n);
-console.log(-9007199254740991999999n - 90071992547409919999n);
+// 2. BigInt
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/BigInt
+// console.log(9007199254740991999999n - 90071992547409919999n);
+// console.log(-9007199254740991999999n - 90071992547409919999n);
 // console.log(-9007199254740991999999n - 90071992547409919999.223n); // err
 
 // console.log(10n - 4); // err
 // console.log(parseInt(10n) - 4); // 6
 // console.log(10n - BigInt(4)); // 6n
 // console.log(5n / 2n); // 2n
+//!========================================================================================================================================================
+
+// 3. Math
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math
+console.log(Math.E); // 2.718281828459045
+console.log(Math.PI); // 3.141592653589793
+
+console.log(Math.sqrt(25)); // 5
+console.log(Math.pow(5, 3)); // 125
+console.log(Math.abs(-42)); // 42
+console.log(Math.max(42, 12, 23, 11, 4232)); // 4232
+console.log(Math.min(42, 12, 23, 11, 4232)); // 11
+console.log(Math.floor(4.9));// Округление вниз. Округляет аргумент до ближайшего меньшего целого. // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
+console.log(Math.ceil(4.9));// Округление вверх. Округляет аргумент до ближайшего большего целого. // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/ceil
+console.log(Math.round(4.9));// Возвращает число, округлённое к ближайшему целому. (5) // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/round
+console.log(Math.trunc(4.9));// Возвращает целую часть числа путём удаления всех дробных знаков. // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/trunc
+console.log(Math.random()); // Рандом
 //!========================================================================================================================================================
