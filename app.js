35462d3f1edab6efd24b0f88c1c5e9d3190407b2
Ruslan_gg <107236487+Navielon@users.noreply.github.com>
2022-07-20 15:49:39 +0300

    upd

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
 // Массивы
+
+const cars = ['Mazda', 'Ford', 'BMW', 'Audi']
+const fib = [1, 1, 2, 3, 5, 8, 13]
+const people = [
+   {name : 'Ruslan', budget: 4200},
+   {name : 'Katya', budget: 4100},
+   {name : 'Vika', budget: 2200}
+]
+console.log(cars);
+
+// Function
+function addItemToEnd(){
+
+}
+
+// Method
+
+// Добавление данных в массив
+// cars.push('Porshe') // В конец
+// console.log(cars);
+// cars.unshift('Reno') // В начало
+// console.log(cars);
+
+// Удаление данных из массива
+
+// const firstCar = cars.shift() // Удаляет из массива и возвращает первый элемент
+// const lastCar = cars.pop() // Удаляет из массива и возвращает последний элемент
+// console.log(firstCar);
+// console.log(lastCar);
+// console.log(cars);
+
+//TODO========================================================================================================================================================
+
+// Reverse 
+// console.log(cars.reverse()); // Разворачивает массив
+
+// Задача 1
+
+// Разворот строки
+// const text = 'Привет, мы изучаем JavaScript'
+// const reverseText = text.split('').reverse().join('')
+// console.log(reverseText);
+//TODO========================================================================================================================================================
+
+// console.log(cars.indexOf('BMW')) // получение индекса элемента из массива
+// const index = cars.indexOf('BMW')
+cars[index] = 'Volkswagen'
+console.log(cars);
+//TODO========================================================================================================================================================
