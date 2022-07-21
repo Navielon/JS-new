97451cf94132fcf322b428d6fb3f1d7c066a9729
Ruslan_gg <107236487+Navielon@users.noreply.github.com>
2022-07-21 12:21:52 +0300

    upd

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // Асинхронность
+
+// const timeout = setTimeout( ()=>{
+//    console.log('After timeout');
+// }, 2500)
+// clearTimeout(timeout)
+
+
+// const interval = setInterval( ()=>{
+//    console.log('After timeout');
+// }, 1500)
+// clearInterval(interval)
+//TODO========================================================================================================================================================
